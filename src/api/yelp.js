ab78e45e291f4e50a8b68b695d78eecb189352e2
Alex Philayvanh <aphilay@gmail.com>
2020-05-10 05:57:40 -0700

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer J8nff1Abzmu9StrNGDwoflHMX01VomnpM1RoA3YHwnGRp0lDSexPlOXSCLhteXlivbtTxfrZ3JqUoxHNhjb4eezrOFdC0g-lbNNosYwy_CcjBkgel8htKTXfnKyOXXYx"
+      "Bearer ADD API KEY HERE"
   }
 });
